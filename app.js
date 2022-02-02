1f94c9cd2227faf4fefe20c0dcbcb832a5241f16
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-02-02 10:54:55 -0800

    changes names and went bacl to demo names. Created render ingredient function and imported functions and grabbed DOM el

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // import functions and grab DOM elements
+// const form = document.getElementById('ingredients');
+const ingredientsIn = document.getElementById('ingredients-in');
+// const quantityIn = document.getElementById('quantity-in');
+// const caloriesIn = document.getElementById('calories-in');
 
-// let state
+// const ingredientBtn = document.getElementById('add-ingredient');
+// const removeBtn = document.getElementById('remove');
+// const saveBtn = document.getElementById('save-meal');
 
+// let state: what are we keeping track of?
+// let calories = [];
+let ingredients = [];
 // set event listeners 
+// eslint-disable-next-line no-unused-vars
+function renderIngredients() {
+    ingredientsIn.textContent = '';
+    for (let ingredient of ingredients) {
+        const li = renderIngredients(ingredient);
+        ingredientsIn.append(li);
+    }
+}
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
